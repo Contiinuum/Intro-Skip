2ea1b85989571d9c13ed6a710bea6ee4eb6ec1ae
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-07-13 02:58:49 +0200

    initial commit

---
+++ src/Hooks.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using MelonLoader;
+using UnityEngine;
+
+namespace AudicaModding
+{
+    internal static class Hooks
+    {
+        public static void ApplyHooks(HarmonyInstance instance)
+        {
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(AudioDriver), "StartPlaying")]
+        private static class PatchStartPlaying
+        {
+            private static void Prefix(AudioDriver __instance)
+            {
+                if (AudicaMod.skipQueued)
+                {
+                    AudicaMod.SkipIntro();
+                }
+            }
+
+        }
+
+        [HarmonyPatch(typeof(OVRInput), "Update")]
+        private static class PatchOVRUpdate
+        {
+            private static bool Prefix(OVRInput __instance)
+            {
+                if (!AudicaMod.introSkip || AudicaMod.introSkipped || AudicaMod.skipQueued) return true;
+
+                if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.All))
+                {
+                    if (AudioDriver.I is null) AudicaMod.QueueSkip();
+                    else AudicaMod.SkipIntro();
+                }
+
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(InGameUI), "Restart")]
+        private static class PatchRestart
+        {
+            private static void Prefix(InGameUI __instance)
+            {
+                AudicaMod.TriggerRestart();
+            }
+        }
+
+
+    }
+}
---
+++ src/Main.cs
@@ -0,0 +1,246 @@
+using System;
+using System.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
+using Harmony;
+
+namespace AudicaModding
+{
+    public class AudicaMod : MelonMod
+    {
+    
+        //for intro skipping
+        static public bool hardcoreFullComboMode = false;
+        static public bool introSkip = false;
+        static public bool introSkipped = false;
+        static public bool isPlaying = false;
+        static public bool skipQueued = false;
+        static public bool canSkip = false;
+        static public GameObject popup;
+        static public VRHardwareSetup.VRHardwareMode hardware;
+        static public string button = "";
+
+
+        //for menu handling
+        public static MenuState.State menuState;
+        public static MenuState.State oldMenuState;
+        public static bool menuSpawned = false;
+        public static OptionsMenuButton toggleButton = null;
+        public static bool miscPageFound = false;
+
+        
+
+        public static class BuildInfo
+        {
+            public const string Name = "IntroSkip";  // Name of the Mod.  (MUST BE SET)
+            public const string Author = "Continuum"; // Author of the Mod.  (Set as null if none)
+            public const string Company = null; // Company that made the Mod.  (Set as null if none)
+            public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
+            public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        }
+
+        private void CreateConfig()
+        {
+            ModPrefs.RegisterPrefBool("IntroSkip", "enabled", false);
+        }
+
+        private void LoadConfig()
+        {
+            introSkip = ModPrefs.GetBool("IntroSkip", "enabled");
+            //MelonModLogger.Log("Loaded config!: " + "IntroSkip is " + (introSkip ? "enabled" : "disabled"));
+        }
+
+        private void SaveConfig()
+        {
+            ModPrefs.SetBool("IntroSkip", "enabled", introSkip);
+            //MelonModLogger.Log("Config saved");
+        }
+
+        public override void OnApplicationStart()
+        {
+            HarmonyInstance instance = HarmonyInstance.Create("AudicaMod");
+            Hooks.ApplyHooks(instance);
+        }
+
+        public override void OnLevelWasLoaded(int level)
+        {
+            if (!ModPrefs.HasKey("IntroSkip", "enabled"))
+            {
+                CreateConfig();
+            }
+            else
+            {
+                LoadConfig();
+            }
+        }
+
+        public static int GetFirstTick()
+        {
+            SongList.SongData songData = SongDataHolder.I.songData;
+            KataConfig.Difficulty diff = KataConfig.I.GetDifficulty();
+            return SongCues.GetCues(songData, diff)[0].tick;
+        }
+
+        public static float GetCurrentTick()
+        {
+            SongList.SongData songData = SongDataHolder.I.songData;
+            return AudioDriver.I.GetTick(AudioDriver.TickContext.Audio);
+        }
+
+        //Queues an intro skip for when AudioDriver is not instantiated yet
+        public static void QueueSkip()
+        {
+            skipQueued = true;
+            //MelonModLogger.Log("Skip queued");
+        }
+
+        public static void SkipIntro()
+        {
+            if (GetCurrentTick() <= GetFirstTick() - 3840)
+            {             
+                AudioDriver.I.JumpToTick(AudicaMod.GetFirstTick() - 1920);
+                AudicaMod.introSkipped = true;
+                AudicaMod.skipQueued = false;
+                //MelonModLogger.Log("Intro skipped!");
+            }
+        }
+
+        public static void TriggerRestart()
+        {
+            //MelonModLogger.Log("Restart triggered");
+            introSkipped = false;
+        }
+
+        public override void OnUpdate()
+        {
+            //Tracking menu state
+            if (MenuState.sState == 0) return;
+
+            menuState = MenuState.GetState();
+            if (menuState != MenuState.State.SettingsPage) miscPageFound = false;
+            if (menuState == MenuState.State.SettingsPage && !miscPageFound)
+            {
+                OptionsMenu[] optionsMenus = GameObject.FindObjectsOfType<OptionsMenu>();
+
+                for (int i = 0; i < optionsMenus.Length; i++)
+                {
+                    if (optionsMenus[i].mPage == OptionsMenu.Page.Misc)
+                    {
+                        miscPageFound = true;
+                    }
+                }
+
+                if (miscPageFound && !menuSpawned)
+                {
+                    for (int i = 0; i < optionsMenus.Length; i++)
+                    {
+                        if (optionsMenus[i].mPage == OptionsMenu.Page.Misc)
+                        {
+
+
+                            string toggleText = "OFF";
+
+                            if (introSkip)
+                            {
+                                toggleText = "ON";
+                            }
+
+                            optionsMenus[i].AddHeader(0, "Skip Intro");
+
+                            toggleButton = optionsMenus[i].AddButton
+                                (0,
+                                toggleText,
+                                new Action(() =>
+                                {
+                                    if (introSkip)
+                                    {
+                                        introSkip = false;
+                                        toggleButton.label.text = "OFF";
+                                        SaveConfig();
+                                    }
+                                    else
+                                    {
+                                        introSkip = true;
+                                        toggleButton.label.text = "ON";
+                                        SaveConfig();
+                                    }
+                                }),
+                                null,
+                                "Skip Intro by pressing:\n" +
+                                "A on Oculus and Index\n" +
+                                "R Menu Button on Vive\n" +
+                                "");
+
+                            menuSpawned = true;
+                        }
+                    }
+                }
+                else if (!miscPageFound)
+                {
+                    menuSpawned = false;
+                }
+
+            }
+
+            if (introSkip)
+            {
+                if (!isPlaying && menuState == MenuState.State.Launched && AudioDriver.I is AudioDriver)
+                {
+                    isPlaying = true;
+                }
+
+                else if (isPlaying && (menuState != MenuState.State.Launched || AudioDriver.I is null))
+                {
+                    isPlaying = false;
+                    introSkipped = false;
+                }
+
+                if (isPlaying)
+                {
+                    if (GetCurrentTick() < GetFirstTick() - 3840 && !skipQueued && !introSkipped)
+                    {
+                        canSkip = true;
+                        if (popup is null || !popup.activeSelf)
+                        {
+                            if (button == "")
+                            {
+                                switch (VRHardwareSetup.I.hardware)
+                                {
+                                    case VRHardwareSetup.VRHardwareMode.Cosmos:
+                                        button = "X";
+                                        break;                              
+                                    case VRHardwareSetup.VRHardwareMode.Knuckles:
+                                    case VRHardwareSetup.VRHardwareMode.OculusNative:
+                                    case VRHardwareSetup.VRHardwareMode.OculusOpenVR:
+                                        button = "A";
+                                        break;
+                                    case VRHardwareSetup.VRHardwareMode.ViveWand:
+                                    case VRHardwareSetup.VRHardwareMode.WinMROpenVR:
+                                        button = "R Menu Button";
+                                        break;
+                                    case VRHardwareSetup.VRHardwareMode.Unknown:
+                                        button = "?";
+                                        break;
+                                }
+                            }
+                            popup = KataConfig.I.CreateDebugText("Skip Intro by pressing <color=#85e359>" + button + "</color>", new Vector3(0f, -1f, 5f), 3f, null, false, 0.001f).gameObject;
+                        }
+                    }
+                    else
+                    {
+                        if (canSkip) canSkip = false;
+                        if (popup is GameObject)
+                        {
+                            GameObject.Destroy(popup);
+                            popup = null;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
